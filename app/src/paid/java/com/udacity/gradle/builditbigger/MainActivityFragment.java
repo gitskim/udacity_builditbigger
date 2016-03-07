7f2d51dd1a95ae46c5da0f5b1a731beaf32fe3a3
Suhyun Kim <Suhyun.Kim@viacommix.com>
2016-03-06 19:31:22 -0500

    Added free and paid flavors

---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,42 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.bgirlogic.joketellinglibrary.JokeTellingAsyncTask;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    private Button mJokeButton;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        mJokeButton = (Button) root.findViewById(R.id.joke_button);
+        setJokeButtonClickListener();
+        return root;
+    }
+
+    private void setJokeButtonClickListener() {
+        mJokeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                JokeTellingAsyncTask jokeTellingAsyncTask = new JokeTellingAsyncTask(getActivity());
+                jokeTellingAsyncTask.execute();
+            }
+        });
+    }
+}
